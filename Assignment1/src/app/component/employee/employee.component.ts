4f470554e452499feb1ac1c298192f9a5c4f86fd
Shyamkadiwar <shyamkadiwar17@gmail.com>
2025-02-21 13:39:05 +0530

    added assignment of components (user, employee, admin)

---
+++ Assignment1/src/app/component/employee/employee.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  imports: [],
+  templateUrl: './employee.component.html',
+  styleUrl: './employee.component.css'
+})
+export class EmployeeComponent {
+  employee = {
+    name: 'Shyam kadiwar',
+    position: 'Full stack developer',
+    department: 'Development'
+  };
+}
---
+++ Assignment1/src/app/component/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  user = {
+    name: 'Shyam kadiwar',
+    age: 21,
+    email: 'shyamkadiwar@gmail.com'
+  };
+}
