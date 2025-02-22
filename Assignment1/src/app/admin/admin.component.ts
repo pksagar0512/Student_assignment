f5d561810cc59d86464c5968e0358bb309a94f7e
Rahevar Mayurdhvajsinh <rahevarmayurdhvajsinh@gmail.com>
2025-02-22 23:33:19 +0530

    Assignment1

---
+++ Assignment1/src/app/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent {
+}
