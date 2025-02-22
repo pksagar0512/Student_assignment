3cd3858e6d60a2552b50c12bef3f8cc6fffee4a2
Supriya Chandra <supriyachandra2003@gmail.com>
2025-02-22 23:09:41 +0530

    assignment1

---
+++ assignment1/src/app/Components/admin/admin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true, // ✅ Must be standalone
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent {}
