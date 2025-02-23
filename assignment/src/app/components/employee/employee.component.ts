b889938b47510d20da431f1da60233f4fe720292
sonalichandra <sonalichandra4686@gmail.com>
2025-02-23 11:48:51 +0530

    Assignment

---
+++ assignment/src/app/components/employee/employee.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  standalone: true,
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {}
