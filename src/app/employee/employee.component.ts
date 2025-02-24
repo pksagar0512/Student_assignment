38e7783ecbd69804d0af0a24735d7ea07d49a31c
codes71 <thuraheinsyh@gmail.com>
2025-02-24 10:56:48 +0530

    Assignment 1 completion

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {}
