d394eedd52eccc19eb5db536ae20366e6e4181e3
Meet7038 <meetkalariya28@gmail.com>
2025-02-24 10:56:04 +0530

    Initial commit with all files

---
+++ assignment/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { UserComponent } from './user/user.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,AdminComponent,EmployeeComponent,UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'assignment';
+}
